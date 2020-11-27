dc0cd6af207efbcf40428b49080b8ae2b8b7eae9
slpbeauty <75103677+slpbeauty@users.noreply.github.com>
2020-11-27 10:16:17 +0700

    Add files via upload

---
+++ data/titikacuanbekasi_1.js
@@ -0,0 +1,8 @@
+var json_titikacuanbekasi_1 = {
+"type": "FeatureCollection",
+"name": "titikacuanbekasi_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 107.001150465320833, -6.241907637573181 ] } }
+]
+}
